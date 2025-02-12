207c35c9698ad89f5e714b406ef17bceb660da60
Bela <75233+sci@users.noreply.github.com>
2025-02-12 10:29:12 +0100

    fix: load functions from R folder

--- _targets.R
+++ _targets.R
@@ -5,16 +5,10 @@ tar_option_set(
   format = "qs",
 )
 
-find_one_file <- function(path, glob) {
-  file <- dir_ls(path, type = "file", glob = glob)
-
-  if (!is_string(file)) {
-    cli_abort("Expected exactly one file matching the pattern {.arg {glob}} in {.path {path}}, but found {?none/}{.file {file}} instead!")
-  }
-
-  cli_alert("found {.file {file}}")
-  file
-}
+tar_config_get("script") |>
+  fs::path_dir() |>
+  fs::path("R") |>
+  tar_source()
 
 list(
   # config ----
