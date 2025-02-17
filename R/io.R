f4dc63d2eec496e4fd269cab4c4a5a9ba01bf18c
Bela <75233+sci@users.noreply.github.com>
2025-02-17 10:30:16 +0100

    style: fix lints for lintr 3.2

--- R/io.R
+++ R/io.R
@@ -1,10 +1,10 @@
 find_one_file <- function(path, glob) {
-  file <- dir_ls(path, type = "file", glob = glob)
+  res <- dir_ls(path, type = "file", glob = glob)
 
-  if (!is_string(file)) {
-    cli_abort("Expected exactly one file matching the pattern {.arg {glob}} in {.path {path}}, but found {?none/}{.file {file}} instead!")
+  if (!is_string(res)) {
+    cli_abort("Expected exactly one file matching the pattern {.arg {glob}} in {.path {path}}, but found {?none/}{.file {res}} instead!")
   }
 
-  cli_alert("found {.file {file}}")
-  file
+  cli_alert("found {.file {res}}")
+  res
 }
