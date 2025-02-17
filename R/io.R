952fce001680fe6271c360b5396e54ca2a03b837
Bela <75233+sci@users.noreply.github.com>
2025-02-17 13:19:44 +0100

    chore: use common io.R from https://github.com/jmf-vienna/amplicon-analysis

--- R/io.R
+++ R/io.R
@@ -1,10 +1,1 @@
-find_one_file <- function(path, glob) {
-  res <- dir_ls(path, type = "file", glob = glob)
-
-  if (!is_string(res)) {
-    cli_abort("Expected exactly one file matching the pattern {.arg {glob}} in {.path {path}}, but found {?none/}{.file {res}} instead!")
-  }
-
-  cli_alert("found {.file {res}}")
-  res
-}
+../../amplicon-analysis/R/io.R
